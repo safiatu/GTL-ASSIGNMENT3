696ae8e4bda74f4318cc44aa0b0e5c1f0e4dd462
safiatu <68543555+safiatu@users.noreply.github.com>
2020-08-21 17:08:51 +0000

    react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Safiatu from './week-3/saf.js';
+
+import './week-3/css/components.css';
+import './week-3/css/global.css';
+import './week-3/css/layout.css';
+import './week-3/css/sample-wallpaper.css';
+
+function App() {
+  return (
+    <div className="App">
+
+     <Safiatu/>
+      </div>
+  );
+}
+
+export default App;
